5c16ef2a363cbd4f96d4b2b1158a2386c521cbac
Ubuntu <ubuntu@ip-172-31-10-188.us-west-2.compute.internal>
2013-07-28 21:07:09 +0000

    correct errorrs

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+	
+var fs = require('fs');
+var fileBuffer = fs.readFileSync('index.html');
+var text = fileBuffer.toString('utf8',0,fileBuffer.length);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
